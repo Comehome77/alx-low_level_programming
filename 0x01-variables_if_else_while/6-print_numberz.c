f79b4db0932bb7a8a20e058509281b42803c0498
Comehome77 <nahomasfaw557@gmail.com>
2023-09-12 06:48:38 -0700

    added 6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-	int num;
-	
-	for (num = 0; num < 10; num++)
-		
+	int num = 0;
+
+	while (num < 10)
+	{
 		putchar(num + '0');
-		putchar('\n');
-		
-		return (0);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
