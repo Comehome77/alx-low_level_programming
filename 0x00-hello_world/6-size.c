7da0c722770c136840f3f1bf9dc5b9cb66ce00f9
Comehome77 <nahomasfaw557@gmail.com>
2023-09-12 00:52:51 -0700

    added 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = sizeof(char);
+	int y = sizeof(int);
+	int z = sizeof(long int);
+	int i = sizeof(long long int);
+	int j = sizeof(float);
+
+	printf("Size of a char: %d byte\(s\)\n", x);
+	printf("Size of a int: %d byte\(s\)\n", y);
+	printf("Size of a long int: %d byte\(s\)\n", z);
+	printf("Size of a long long int: %d byte\(s\)\n", i);
+	printf("Size of a float: %d byte\(s\)\n", j);
+	return (0);
+}
