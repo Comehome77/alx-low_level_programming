03b73d9700a8187d35e63fe3440156b85e9e96a2
Comehome77 <nahomasfaw557@gmail.com>
2023-09-12 07:22:50 -0700

    added 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m = 0;
+
+	while (m < 10)
+	{
+		putchar(m + '0');
+		{
+			if (m < 9)
+		putchar(',');
+		putchar(' ');
+		}
+		m++;
+	}
+	putchar('\n');
+
+	return (0);
+}
