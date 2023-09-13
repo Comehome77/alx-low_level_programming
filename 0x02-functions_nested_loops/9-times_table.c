e90ccaf3fa16ab0cd15b11b52babc9a1a6b0a4cf
Comehome77 <nahomasfaw557@gmail.com>
2023-09-13 08:38:33 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - gives the time table of numbers
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z <= 9)
+			{
+			_putchar(z + '0');
+			}
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			if (z != 81)
+			{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		}
+		_putchar('\n');
+	}
+}
