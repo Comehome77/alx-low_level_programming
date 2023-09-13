c3413421e797c63a7b71fcc23ccff620b703944d
Comehome77 <nahomasfaw557@gmail.com>
2023-09-13 12:39:48 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,21 +8,19 @@
 
 void times_table(void)
 {
-	int x;
-	int y;
-	int z;
-
+	int x,y,z;
+	
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-			if (z <= 9 && y != 9 && y !=0)
+			if (z <= 9 && y != 9 && y != 0)
 			{
 			_putchar(' ');
 			_putchar(z + '0');
 			_putchar(',');
-			_putchar(' ');			
+			_putchar(' ');
 			}
 			else if (y == 0)
 			{
@@ -45,7 +43,7 @@ void times_table(void)
 			else if (z > 9 && y == 9)
 			{
 				_putchar(z / 10 + '0');
-                                _putchar(z % 10 + '0');
+				_putchar(z % 10 + '0');
 			}
 		}
 		_putchar('\n');
