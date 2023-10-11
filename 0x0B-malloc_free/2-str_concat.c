8dc0277700e745b1014b1d44fc91f40972b8237b
Comehome77 <nahomasfaw557@gmail.com>
2023-10-11 16:19:31 -0700

    added files

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concatenates strings
+ * @s1: string one
+ * @s2: string two
+ * Return: result or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int a, b, c, d;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (b = 0; s1[b] != '\0'; b++);
+
+	for (a = 0; s2[a] != '\0'; a++);
+
+	res = malloc(sizeof(char) * (a + b + 1));
+
+	if (res == NULL)
+	{
+		free(result);
+		return (NULL);
+	}
+
+	for (b = 0; c < b; c++)
+		result[c] = s1[c];
+
+	z = w;
+	for (a = 0; a <= d; c++, a++)
+		result[c] = s2[a];
+
+	return (result);
+}
