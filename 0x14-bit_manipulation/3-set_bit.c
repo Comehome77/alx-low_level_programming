3a30f32c89b053eb06612e9d7924a9c72e1f4b31
Comehome77 <nahomasfaw557@gmail.com>
2023-10-26 14:07:40 -0700

    added 3-set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to 1
+ * @n: decimal number
+ * @index: position
+ * Return: 1 or -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int number = 1;
+
+	number <<= index;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	(*n) = (*n) | number;
+	return (1);
+}
