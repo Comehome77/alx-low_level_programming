2309fc7206d4e349a96afd7c8f379d7d2bfb698f
Comehome77 <nahomasfaw557@gmail.com>
2023-10-26 14:14:45 -0700

    added 100-get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endianness
+ * Return: 1 or 0
+ */
+
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *b = (char *)&a;
+
+	if (*a)
+		return (1);
+	else
+		return (0);
+}
